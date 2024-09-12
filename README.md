c91ac06d3ed0eeae2eda001490f58d35d812b745
Karan Jadhav <karan.jadhavhkr@gmail.com>
2024-09-12 13:40:42 +0530

    upgrade to new

---
+++ README.md
@@ -0,0 +1,8 @@
+# Self Host Labs Blog
+
+This is the source code for the Self Host Labs blog. To run the blog locally run the following commands:
+
+```bash
+yarn install
+yarn start
+```
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: 'https://jadhav.dev', title: 'About' },
+]
+
+export default headerNavLinks
